d6a703b5dbb228a508978a62c8fa8ea2076b7978
FrancisCastillo24 <162831161+FrancisCastillo24@users.noreply.github.com>
2025-08-23 23:38:02 +0200

    Clase Product
    He creado y desarrollando MVC de product

--- app/Livewire/Admin/Products/ProductCreate.php
+++ app/Livewire/Admin/Products/ProductCreate.php
@@ -4,15 +4,23 @@ namespace App\Livewire\Admin\Products;
 
 use App\Models\Category;
 use App\Models\Family;
+use App\Models\Product;
+use App\Models\Subcategory;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ProductCreate extends Component
 {
-    // Defino una propiedad de producto y de la familia
+    // Con esta librería, la aplicación está lista para subir imágenes
+    use WithFileUploads;
+
+    // Defino las propiedades
     public $families;
     public $family_id = '';
-    public $category_idº = '';
+    public $category_id = '';
+    public $image;
+
     public $product = [
         'sku' => '',
         'name' => '',
@@ -27,10 +35,68 @@ class ProductCreate extends Component
         $this->families = Family::all();
     }
 
+    public function boot()
+    {
+        $this->withValidator(function ($validator) {
+            if ($validator->fails()) {
+
+                $this->dispatch('swal', [
+                    'icon' => 'success',
+                    'title' => '¡Error!',
+                    'text' => 'El formulario contiene errores.',
+                ]);
+            }
+        });
+    }
+
+    // Método para que al seleccionar una categoría o una familia, el resto de select se limpie
+    public function updatedFamilyId($value)
+    {
+        $this->category_id = '';
+        $this->product['subcategory_id'] = '';
+    }
+
+    // Método para que al seleccionar una categoría nueva, la subcategoría se limpia
+    public function updatedCategoryId($value)
+    {
+        $this->product['subcategory_id'] = '';
+    }
+
     #[Computed()]
     public function categories()
     {
-        return Category::where('family_id', $this->family_id, $this->family_id)->get();
+        return Category::where('family_id', $this->family_id)->get();
+    }
+
+    #[Computed()]
+    public function subcategories()
+    {
+        return Subcategory::where('category_id', $this->category_id)->get();
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'image' => 'required|image|max:1024',
+            'product.sku' => 'required|unique:products,sku',
+            'product.name' => 'required|max:255',
+            'product.description' => 'nullable',
+            'product.price' => 'required|numeric|min:0',
+            'product.subcategory_id' => 'required|exists:subcategories,id',
+        ]);
+
+        $this->product['image_path'] = $this->image->store('products');
+
+        $product = Product::create($this->product);
+
+        session()->flash('swal', [
+            'icon' => 'success',
+            'title' => '¡Bien Hecho!',
+            'text' => 'Producto creado correctamente.'
+        ]);
+
+
+        return redirect()->route('admin.products.edit', $product);
     }
 
     public function render()
--- resources/views/livewire/admin/products/product-create.blade.php
+++ resources/views/livewire/admin/products/product-create.blade.php
@@ -1,43 +1,90 @@
 <div>
-    <!-- Tarjeta para Código -->
-    <div class="card">
-        <div class="mb-4">
-            <x-label class="mb-1">Código</x-label>
-            <x-input wire:model="product.sku" class="w-full" placeholder="Por favor ingrese el código del producto" />
+
+    <form wire:submit="store">
+
+        <figure class="mb-4 relative">
+
+            <div class="absolute top-8 right-8">
+                <label class="flex items-center px-4 py-2 rounded-lg bg-gray-400 cursor-pointer text-black">
+                    <i class="fas fa-camera mr-2"></i>
+                    Actualizar imagen
+                    <input type="file" class="hidden" accept="image/*" wire:model="image">
+                </label>
+            </div>
+            <img class="w-full h-96 object-contain rounded-lg border"
+                src="{{ $image ? $image->temporaryUrl() : asset('img/no-image.png') }}" alt="">
+        </figure>
+
+        <x-validation-errors class="mb-4"></x-validation-errors>
+
+        <!-- Tarjeta para Código -->
+        <div class="card">
+
+
+            <div class="mb-4">
+                <x-label class="mb-1">Código</x-label>
+                <x-input wire:model="product.sku" class="w-full"
+                    placeholder="Por favor ingrese el código del producto" />
+            </div>
+
+            <!-- Tarjeta para Nombre -->
+            <div class="mb-4">
+                <x-label class="mb-1">Nombre</x-label>
+                <x-input wire:model="product.name" class="w-full"
+                    placeholder="Por favor ingrese el nombre del producto" />
+            </div>
+
+            <!-- Tarjeta para Descripción -->
+            <div class="mb-4">
+                <x-label class="mb-1">Descripción</x-label>
+                <x-textarea wire:model="product.description" class="w-full"
+                    placeholder="Por favor ingrese la descripción del producto"></x-textarea>
+            </div>
+
+            <!-- Tarjeta para Familias -->
+            <div class="mb-4">
+                <x-label class="mb-1">Familias</x-label>
+                <x-select class="w-full" wire:model.live="family_id">
+                    <option value="" disabled>Seleccione una familia</option>
+                    @foreach ($families as $family)
+                        <option value="{{ $family->id }}">{{ $family->name }}</option>
+                    @endforeach
+                </x-select>
+            </div>
+
+            <!-- Tarjeta para Categorías -->
+            <div class="mb-4">
+                <x-label class="mb-1">Categorías</x-label>
+                <x-select class="w-full" wire:model.live="category_id">
+                    <option value="" disabled>Seleccione una categoría</option>
+                    @foreach ($this->categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </x-select>
+            </div>
+
+            <!-- Tarjeta para Subcategoría -->
+            <div class="mb-4">
+                <x-label class="mb-1">Subcategorías</x-label>
+                <x-select class="w-full" wire:model.live="product.subcategory_id">
+                    <option value="" disabled>Seleccione una subcategoría</option>
+                    @foreach ($this->subcategories as $subcategory)
+                        <option value="{{ $subcategory->id }}">{{ $subcategory->name }}</option>
+                    @endforeach
+                </x-select>
+            </div>
+
+            <div class="mb-4">
+                <x-label class="mb-1">Precio</x-label>
+                <x-input type="number" step="0.01" wire:model="product.price" class="w-full"
+                    placeholder="Por favor ingrese el precio del producto" />
+            </div>
+
+            <div class="flex justify-end">
+                <x-button> Crear producto </x-button>
+            </div>
+
         </div>
 
-        <!-- Tarjeta para Nombre -->
-        <div class="mb-4">
-            <x-label class="mb-1">Nombre</x-label>
-            <x-input wire:model="product.name" class="w-full" placeholder="Por favor ingrese el nombre del producto" />
-        </div>
-
-        <!-- Tarjeta para Descripción -->
-        <div class="mb-4">
-            <x-label class="mb-1">Descripción</x-label>
-            <x-textarea wire:model="product.description" class="w-full"
-                placeholder="Por favor ingrese la descripción del producto"></x-textarea>
-        </div>
-
-        <!-- Tarjeta para Familias -->
-        <div class="mb-4">
-            <x-label class="mb-1">Familias</x-label>
-            <x-select class="w-full" wire:model.live="family_id">
-                <option value="" disabled>Seleccione una familia</option>
-                @foreach ($families as $family)
-                    <option value="{{ $family->id }}">{{ $family->name }}</option>
-                @endforeach
-            </x-select>
-        </div>
-
-        <!-- Tarjeta para Categorías -->
-        <div class="mb-4">
-            <x-label class="mb-1">Categoría</x-label>
-            <x-select class="w-full" wire:model.live="category_id">
-                <option value="" disabled>Seleccione una categoría</option>
-                @foreach ($this->categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
-            </x-select>
-        </div>
-    </div>
+    </form>
+</div>
