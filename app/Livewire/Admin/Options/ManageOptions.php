9642030f4ab2f77bc0b1ccf21b9ebb0ad3a7a39e
FrancisCastillo24 <162831161+FrancisCastillo24@users.noreply.github.com>
2025-09-14 12:22:59 +0200

    Creando las opciones
    Desarrollo el livewire de option.

--- app/Livewire/Admin/Options/ManageOptions.php
+++ app/Livewire/Admin/Options/ManageOptions.php
@@ -9,9 +9,33 @@ class ManageOptions extends Component
 {
     public $options;
 
+    public $newOption = [
+        'name' => '',
+        'type' => 1,
+        'features' => [
+            [
+                'value' => '',
+                'description' => '',
+            ]
+        ]
+    ];
+
+    // Cuando pulsemos nueva opción, se abre el modal. Mientras no lo pulsemos, muestra la vista de opciones directamente.
+    public $openModal = true;
+
     public function mount()
     {
         $this->options = Option::with('features')->get();
+    }
+
+    // Botón agregar valor
+    public function addFeature()
+    {
+        // Cuando pulsemos el botón agregar valor, agrega un nuevo feature (array)
+        $this->newOption['features'][] = [
+            'value' => '',
+            'description' => '',
+        ];
     }
 
     public function render()
--- resources/views/livewire/admin/options/manage-options.blade.php
+++ resources/views/livewire/admin/options/manage-options.blade.php
@@ -1,48 +1,138 @@
 <div>
+
     <section class="rounded-lg bg-white shadow-lg">
+
         <header class="border-b border-gray-200 px-6 py-2">
-            <h1 class="text-lg font-semibold text-gray-700">Opciones</h1>
+
+            <div class="flex justify-between">
+                <h1 class="text-lg font-semibold text-gray-700">Opciones</h1>
+                <x-button wire:click="$set('openModal', true)">Nuevo</x-button>
+            </div>
+
         </header>
 
         <div class="p-6">
             <div class="space-y-6">
                 @foreach ($options as $option)
-                <div class="relative p-6 rounded-lg border border-gray-200">
-                    <!-- Etiqueta flotante -->
-                    <div class="absolute -top-3 left-4 bg-white px-4">
-                        <span class="text-gray-600 font-medium">
-                            {{ $option->name }}
-                        </span>
+                    <div class="relative p-6 rounded-lg border border-gray-200">
+                        <!-- Etiqueta flotante -->
+                        <div class="absolute -top-3 left-4 bg-white px-4">
+                            <span class="text-gray-600 font-medium">
+                                {{ $option->name }}
+                            </span>
+                        </div>
+
+                        <!-- Valores -->
+                        <div class="flex flex-wrap gap-2">
+                            @foreach ($option->features as $feature)
+                                @switch($option->type)
+                                    @case(1)
+                                        <span class="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-sm">
+                                            {{ $feature->description }}
+                                        </span>
+                                    @break
+
+                                    @case(2)
+                                        <span class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4"
+                                            style="background-color: {{ $feature->value }};">
+
+                                        </span>
+                                    @break
+
+                                    @default
+                                        <span class="bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded-sm">
+                                            {{ $feature->description }}
+                                        </span>
+                                @endswitch
+                            @endforeach
+                        </div>
                     </div>
-
-                    <!-- Valores -->
-                    <div class="flex flex-wrap gap-2">
-                        @foreach ($option->features as $feature)
-                        @switch($option->type)
-                        @case(1)
-                        <span class="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-sm">
-                            {{ $feature->description }}
-                        </span>
-                        @break
-
-                        @case(2)
-                        <span class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4" style="background-color: {{ $feature->value }};">
-
-                        </span>
-                        @break
-
-                        @default
-                        <span class="bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded-sm">
-                            {{ $feature->description }}
-                        </span>
-                        @endswitch
-                        @endforeach
-                    </div>
-                </div>
                 @endforeach
 
 
             </div>
         </div>
     </section>
+
+    <!-- Llamamos a la variable openModal de Livewire ManageOptions -->
+    <x-dialog-modal wire:model="openModal">
+
+        <!-- Hace referencia a la variable del componente dialog-modal-->
+        <x-slot name="title">Crear nueva opción</x-slot>
+        <x-slot name="content">
+
+            <div class="grid grid-cols-2 gap-6 mb-4">
+
+                <div>
+
+                    <x-label class="mb-1">Nombre</x-label>
+
+                    <x-input class="w-full" wire:model="newOption.name"
+                        placeholder="Por ejemplo: Tamaño, Color"></x-input>
+
+                </div>
+
+
+                <div>
+
+                    <x-label class="mb-1">Tipo</x-label>
+
+                    <x-select wire:model="newOption.type" class="w-full">
+                        <option value="1">Texto</option>
+                        <option value="2">Color</option>
+                        </x-input>
+
+                </div>
+
+            </div>
+
+
+            <div class="flex items-center mb-4">
+
+                <hr class="flex-1">
+
+                <span class="mx-4">Valores</span>
+
+                <hr class="flex-1">
+
+            </div>
+
+            <div class="mb-4 space-y-4">
+
+                @foreach ($newOption['features'] as $index => $feature)
+                <!-- Agregamos la llave key para hacer un seguimiento-->
+                    <div class="p-6 rounded-lg border border-gray-200" wire:key="features{{ $index }}">
+                        <div class="grid grid-cols-2 gap-6">
+
+                            <div>
+                                <x-label class="mb-1">Valor</x-label>
+
+                                <x-input wire:model="newOption.features.{{ $index }}.value" class="w-full"
+                                    placeholder="Ingrese un valor de la opción"></x-input>
+                            </div>
+
+                            <div>
+                                <x-label class="mb-1">Descripción</x-label>
+                                <x-input wire:model="newOption.features.{{ $index }}.value" class="w-full"
+                                    placeholder="Ingrese una descripción"></x-input>
+                            </div>
+
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+
+            <div class="flex justify-end">
+
+                <x-button wire:click="addFeature">Agregar valor</x-button>
+
+            </div>
+
+
+
+        </x-slot>
+        <x-slot name="footer"></x-slot>
+
+    </x-dialog-modal>
 </div>
